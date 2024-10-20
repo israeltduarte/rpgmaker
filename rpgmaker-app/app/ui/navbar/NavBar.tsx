fffa8bf1a172361b0852e04a79aedb7ad773879f
Israel Terra Duarte <israeltduarte@hotmail.com>
2024-10-20 18:49:47 -0300

    fix color arrangement in navbar

--- rpgmaker-app/app/ui/navbar/NavBar.tsx
+++ rpgmaker-app/app/ui/navbar/NavBar.tsx
@@ -4,9 +4,18 @@ import { signOut } from '@/app/lib/actions';
 import Link from 'next/link';
 import { useState } from 'react';
 
+// Definindo as cores como constantes
+const COLORS = {
+  primary: 'bg-indigo-600',
+  hoverPrimary: 'hover:bg-indigo-700',
+  hoverSubMenu: 'hover:bg-indigo-500',
+  subMenuBackground: 'bg-indigo-400',
+  textColor: 'text-white',
+};
+
 const NavItem: React.FC<{ href: string; children: React.ReactNode; isSubMenu?: boolean; }> = ({ href, children, isSubMenu = false }) => {
-  const itemClasses = `block w-full h-full text-white flex items-center justify-center p-4 transition-colors duration-300 ${isSubMenu ? 'hover:bg-indigo-600' : 'hover:bg-indigo-700'}`;
-  const divClasses = isSubMenu ? '' : 'm-0 flex items-center h-16 hover:bg-indigo-700 transition-colors duration-300';
+  const divClasses = `${isSubMenu ? '' : 'm-0 flex items-center h-16 ' + COLORS.hoverPrimary} transition-colors duration-300`;
+  const itemClasses = `${COLORS.textColor} ${isSubMenu ? COLORS.hoverSubMenu : COLORS.hoverPrimary} block w-full h-full flex items-center justify-center p-4 transition-colors duration-300`;
 
   return (
     <div className={divClasses}>
@@ -22,24 +31,24 @@ const Navbar = () => {
   const [isCharactersMenuOpen, setIsCharactersMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-indigo-600">
+    <nav className={COLORS.primary}>
       <div className="container mx-auto flex justify-between items-center h-16 relative">
-        <h1 className="text-white text-xl font-bold">
+        <h1 className={COLORS.textColor + " text-xl font-bold"}>
           <Link href="/dashboard">
-            <span className="text-white hover:text-indigo-200 transition-colors duration-300">rpgmaker</span>
+            <span className={COLORS.textColor + " hover:text-indigo-200 transition-colors duration-300"}>rpgmaker</span>
           </Link>
         </h1>
 
         <div className={`lg:flex lg:space-x-4 items-center justify-center flex-grow hidden lg:flex`}>
           {/* Submenu de Cidades */}
           <div
-            className={`relative group h-16 flex items-center hover:bg-indigo-700`}
+            className={`relative group h-16 flex items-center ${COLORS.hoverPrimary}`}
             onMouseEnter={() => setIsCitiesMenuOpen(true)}
             onMouseLeave={() => setIsCitiesMenuOpen(false)}
           >
             <NavItem href="/dashboard/cities">Cidades</NavItem>
             {isCitiesMenuOpen && (
-              <div className="absolute left-0 top-full bg-indigo-500 text-white rounded shadow-lg w-52 z-50">
+              <div className={`absolute left-0 top-full ${COLORS.subMenuBackground} ${COLORS.textColor} rounded shadow-lg w-52 z-50`}>
                 <NavItem href="/dashboard/cities/view-cities" isSubMenu>Ver Cidades</NavItem>
                 <NavItem href="/dashboard/cities/add-city" isSubMenu>Adicionar Cidade</NavItem>
               </div>
@@ -48,13 +57,13 @@ const Navbar = () => {
 
           {/* Submenu de Personagens */}
           <div
-            className={`relative group h-16 flex items-center hover:bg-indigo-700`}
+            className={`relative group h-16 flex items-center ${COLORS.hoverPrimary}`}
             onMouseEnter={() => setIsCharactersMenuOpen(true)}
             onMouseLeave={() => setIsCharactersMenuOpen(false)}
           >
             <NavItem href="/dashboard/characters">Personagens</NavItem>
             {isCharactersMenuOpen && (
-              <div className="absolute left-0 top-full bg-indigo-500 text-white rounded shadow-lg w-52 z-50">
+              <div className={`absolute left-0 top-full ${COLORS.subMenuBackground} ${COLORS.textColor} rounded shadow-lg w-52 z-50`}>
                 <NavItem href="/dashboard/characters/view-characters" isSubMenu>Ver Personagens</NavItem>
                 <NavItem href="/dashboard/characters/add-character" isSubMenu>Adicionar Personagem</NavItem>
               </div>
